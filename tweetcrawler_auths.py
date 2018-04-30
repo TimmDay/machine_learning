289a20bb538cda247d3ec3b1313f05efb9464670
Tim <timmday.info@gmail.com>
2018-04-30 14:26:15 +0200

    Update tweetcrawler_auths.py

--- tweetcrawler_auths.py
+++ tweetcrawler_auths.py
@@ -1,15 +1,11 @@
 """
 Simple twitter crawler program to gather Hungarian tweets
 Authentication and twitter access
-
-Author: Tim Day, Réka Juhász
-Assignment 1, Statistical Natural Language Processing course
-University of Tübingen, 2018
 """
 import tweepy
 
-auth = tweepy.OAuthHandler('ldZdf11vczxzyPcqVdXjbjKqF', '3dms2CXd7QnphABKx1jpTqw9kEiSLS82SWhBowdBvAFJPVRzKL')
-auth.set_access_token('2312415672-jkPJ8x6CysNMkwxXvGjwAgntXsgisdHxMMWniBU', 'k755hrhZLHkes8KP7nqZfEtayCxOPCX8maJqXnSEa6A8m')
+auth = tweepy.OAuthHandler('#####', '#####')
+auth.set_access_token('#-###', '#####')
 
 api = tweepy.API(auth, wait_on_rate_limit=True)
 
